2f20794594132bf20bc9fef09128610200902103
remy-cannaya <61230367+remy-cannaya@users.noreply.github.com>
2021-01-13 12:20:18 +0100

    Update test_simu_automobile.c

--- test_simu_automobile.c
+++ test_simu_automobile.c
@@ -6,8 +6,8 @@
 
 #include "CUnit.h"
 #include "Basic.h"
-//#include "CUnit/Automated.h"
-//#include "CUnit/Console.h"
+#include "Automated.h"
+#include "CUnit/Console.h"
 
 
 
@@ -135,20 +135,20 @@ int main ( void )
       return CU_get_error();
    }
 
-   // Run all tests using the basic interface
+  /* // Run all tests using the basic interface
    CU_basic_set_mode(CU_BRM_VERBOSE);
    CU_automated_run_tests();
    printf("\n");
    CU_basic_show_failures(CU_get_failure_list());
-   printf("\n\n");
-/*
+   printf("\n\n");*/
+
    // Run all tests using the automated interface
    CU_automated_run_tests();
    CU_list_tests_to_file();
 
    // Run all tests using the console interface
    CU_console_run_tests();
-*/
+
    /* Clean up registry and return */
    CU_cleanup_registry();
    return CU_get_error();
